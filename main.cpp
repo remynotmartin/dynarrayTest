77a672553e230a56bd9ab0a14e81335501f990a7
Remy Dinh <remy_dinh@yahoo.com>
2019-02-03 05:27:30 -0800

    Added main file

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <chrono>
+#include <cstdio>
+#include <ctime> // For the time() and srand() functions
+
+using std::chrono;
+using std::cout;
+using std::cerr;
+
+/*
+    Task: Fill an array of 5000 Elements with random numbers,
+          then print every 500th number to the console.
+*/
+
+int main () {
+    return 0;
+}
